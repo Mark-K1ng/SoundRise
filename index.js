23040e0f04b32ac039ae018a1fe0da4a559931dd
Mark-K1ng <ghilgames17k@gmail.com>
2024-05-02 20:14:21 +0300

    Commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 document.querySelector(".icon").addEventListener("click", function() {
-    var slidingbar = document.querySelector(".slidingbar");
-    if (slidingbar.style.left === "-300px") {
-      slidingbar.style.left = "0px"; // Slide in
-    } else {
-      slidingbar.style.left = "-300px"; // Slide out
-    }
-  });
+  var slidingbar = document.querySelector(".slidingbar");
+  var slidingbarLeft = window.getComputedStyle(slidingbar).getPropertyValue("left");
+  if (slidingbarLeft === "-300px") {
+      slidingbar.style.left = "0px";
+  } else {
+      slidingbar.style.left = "-300px";
+  }
+});
+
+
+function HeightFinder() {
+  var navbarHeight = document.querySelector('.navbar').offsetHeight;
+  var musicbarHeight = document.querySelector('.musicbar').offsetHeight;
   
+  document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
+  document.documentElement.style.setProperty('--musicbar-height', musicbarHeight + 'px');
+}
+
+HeightFinder();
