b85b83ea86d2f8dc1051d60ea9b83a58998216a4
Mark-K1ng <ghilgames17k@gmail.com>
2024-05-03 02:07:31 +0300

    Commit

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-document.querySelector(".icon").addEventListener("click", function() {
-  var slidingbar = document.querySelector(".slidingbar");
-  var slidingbarLeft = window.getComputedStyle(slidingbar).getPropertyValue("left");
-  if (slidingbarLeft === "-300px") {
-      slidingbar.style.left = "0px";
-  } else {
-      slidingbar.style.left = "-300px";
-  }
-});
-
-
 function HeightFinder() {
   var navbarHeight = document.querySelector('.navbar').offsetHeight;
   var musicbarHeight = document.querySelector('.musicbar').offsetHeight;
@@ -16,5 +5,26 @@ function HeightFinder() {
   document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
   document.documentElement.style.setProperty('--musicbar-height', musicbarHeight + 'px');
 }
-
 HeightFinder();
+
+
+function WidthFinder() {
+  var slidingbarWidth = document.querySelector('.slidingbar').offsetWidth;
+  var negSlidingbarWidth = slidingbarWidth * -1;
+
+  document.documentElement.style.setProperty('--SBWidth', slidingbarWidth + 'px');
+  document.documentElement.style.setProperty('--NSBWidth', negSlidingbarWidth + 'px');
+}
+WidthFinder();
+
+
+document.querySelector(".icon").addEventListener("click", function() {
+  var slidingbar = document.querySelector(".slidingbar");
+  var slidingbarLeft = window.getComputedStyle(slidingbar).getPropertyValue("left");
+  
+  if (slidingbarLeft === "0px") {
+    slidingbar.style.left = `var(--NSBWidth)`;
+  } else {
+    slidingbar.style.left = "0px";
+  }
+});
