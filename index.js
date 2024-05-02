537847d505d7b330a26fc776e535964dcec936e7
Mark-K1ng <ghilgames17k@gmail.com>
2024-05-02 15:44:17 +0300

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var a = 7
+document.querySelector(".icon").addEventListener("click", function() {
+    var slidingbar = document.querySelector(".slidingbar");
+    if (slidingbar.style.left === "-300px") {
+      slidingbar.style.left = "0px"; // Slide in
+    } else {
+      slidingbar.style.left = "-300px"; // Slide out
+    }
+  });
+  
