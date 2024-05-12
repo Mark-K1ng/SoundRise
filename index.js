afdf3cc897d2bf7ebb0d4527c84b9b3f26ebf823
Mark-K1ng <ghilgames17k@gmail.com>
2024-05-12 04:12:46 +0300

    Commit

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+fetch('src/data.json')
+  .then(response => response.json())
+  .then(data => {
+    const song1Name = data.song1.name;
+    const song1Img = data.song1.img;
+    const song1NameElement = document.getElementById('song1Name');
+    const song1ImgElement = document.getElementById('song1Img');
+    song1NameElement.textContent = song1Name;
+    song1ImgElement.setAttribute('src', song1Img);
+    song1ImgElement.setAttribute('alt', song1Name);
+
+    const song2Name = data.song2.name;
+    const song2Img = data.song2.img;
+    const song2NameElement = document.getElementById('song2Name');
+    const song2ImgElement = document.getElementById('song2Img');
+    song2NameElement.textContent = song2Name;
+    song2ImgElement.setAttribute('src', song2Img);
+    song2ImgElement.setAttribute('alt', song2Name);
+})
+
+
 function HeightFinder() {
-  var navbarHeight = document.querySelector('.navbar').offsetHeight;
-  var musicbarHeight = document.querySelector('.musicbar').offsetHeight;
-  var textHeight = document.querySelector('.song p').offsetHeight
-  var pointHeight = document.querySelector('.progressingbar').offsetHeight - (document.querySelector('.point').offsetHeight / 2);
+  window.addEventListener('load', () => {
+    var navbarHeight = document.querySelector('.navbar').offsetHeight;
+    var musicbarHeight = document.querySelector('.musicbar').offsetHeight;
+    var textHeight = document.querySelector('.song p').offsetHeight;
+    var pointHeight = document.querySelector('.progressingbar').offsetHeight - (document.querySelector('.point').offsetHeight / 2);
   
-  document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
-  document.documentElement.style.setProperty('--musicbar-height', musicbarHeight + 'px');
-  document.documentElement.style.setProperty('--text-height', textHeight + 'px');
-  document.documentElement.style.setProperty('--point-height', pointHeight + 'px');
+    document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
+    document.documentElement.style.setProperty('--musicbar-height', musicbarHeight + 'px');
+    document.documentElement.style.setProperty('--text-height', textHeight + 'px');
+    document.documentElement.style.setProperty('--point-height', pointHeight + 'px');
+  });
 }
 HeightFinder();
+
 
 
 function WidthFinder() {
@@ -38,8 +62,7 @@ function PositionFinder() {
 PositionFinder();
 
 
-function ButtonToggle() {
-  
+function ButtonToggle() { 
   document.getElementById('playbutton').addEventListener('click', function() {
     var iconElement = document.getElementById('playbutton');
     if (iconElement.classList.contains('fa-play')) {
@@ -61,9 +84,8 @@ ButtonToggle();
 
 
 window.onload = function() {
-
   WidthFinder();
-
+  
   document.querySelector(".icon").addEventListener("click", function() {
     var slidingbar = document.querySelector(".slidingbar");
     var slidingbarLeft = window.getComputedStyle(slidingbar).getPropertyValue("left");
@@ -78,3 +100,4 @@ window.onload = function() {
   var a = 0;
   console.log(a);
 };
+
